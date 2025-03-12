c906d277725434fe334b20871b8a4c04c4a9edaa
Lene-Lise <lenelise@gmail.com>
2025-03-12 15:04:45 +0100

    added new field with predefined choices in Income model

--- mysecondproject/expensetracker/models.py
+++ mysecondproject/expensetracker/models.py
@@ -23,13 +23,20 @@ class ExpenseCategory(models.Model):
         return str(self.title)
     
 class Income(models.Model):
+
+    class IncomeTypes(models.TextChoices):
+        MAIN = 'M', 'MainIncome'
+        SIDE = 'S', 'SideIncome'
+        GIFT = 'G', 'Gift'
+
     title = models.CharField(max_length=30)
     content = models.TextField(blank=True) #optional to fill this out
     price = models.IntegerField(validators=[MinValueValidator(0)])
     owner = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE) #we dont want to keep the expense if the owner is deleted
     date = models.DateField()
+    income_type = models.CharField(max_length=1, choices=IncomeTypes.choices, default='')
     budget = models.ForeignKey('Budget',on_delete=models.SET_NULL, null=True)
-
+    
     def __str__(self):
         return str(self.title) + ":" + str(self.price) + "."
     
